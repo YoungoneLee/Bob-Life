92d286aba4339d46ce39c6c44f605c0a1fcb560f
Cole Lee <clee9@trinity.edu>
2022-11-15 22:17:29 -0600

    working doodle bob game

---
+++ Assets/Scripts/doodleBob/DoodleEndScript.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class DoodleEndScript : MonoBehaviour
+{
+    public float score;
+    public Text Score;
+
+    public void loadDoodleGame() {
+        SceneManager.LoadScene("JumpingScene");
+        PlayerPrefs.SetFloat("score", 0);
+    }
+    public void loadHubScene() {
+        SceneManager.LoadScene("HubScene");
+        PlayerPrefs.SetFloat("score",0);
+    }
+
+    public void Start() {
+        score = PlayerPrefs.GetFloat("score");
+        Score.text = ("Score: " + score); 
+    }
+}
