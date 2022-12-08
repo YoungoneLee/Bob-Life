7e7db430b7e0e0499f7996df91098e23ada60d6d
Youngone Lee <youngone.lee@accenture.com>
2022-12-07 22:34:38 -0600

    working on the score

--- Assets/ScoreSystem.cs
+++ Assets/ScoreSystem.cs
@@ -1,18 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ScoreSystem : MonoBehaviour
 {
+    public int score = 0;
+    public Text scoreText;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            score += 1;
+            scoreText.text = "Score : " + score;
+        }
     }
 }
