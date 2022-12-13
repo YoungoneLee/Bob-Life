8679325aa18090197a4010527cf869fb4ede943b
Youngone Lee <youngone.lee@accenture.com>
2022-12-13 01:54:49 -0600

    charcter controller in use

--- Assets/Scripts/newRunningGame/PlayerController.cs
+++ Assets/Scripts/newRunningGame/PlayerController.cs
@@ -25,18 +25,33 @@ public class PlayerController : MonoBehaviour
     private int speeds;
     private float increase;
 
+    //fixing gravity
+    public CharacterController character;
+    public float gravity = 9.81f * 2.0f;
+    private Vector3 direction;
+
     // Start is called before the first frame update
     void Start()
     {
-        rb2d = GetComponent<Rigidbody2D>();
+        direction = Vector3.zero;
+        character = GetComponent<CharacterController>();
+        //rb2d = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
         StartCoroutine("IncreaseGameSpeed");
         PlayerPrefs.GetInt("speed");
     }
 
+    //private void OnEnable()
+    //{
+    //}
+
     private void FixedUpdate()
     {
-        if(rb2d.velocity.y < 0)
+        //rb2d.AddForce(Vector3.down * gravity * Time.deltaTime);
+        direction += Vector3.down * gravity * Time.deltaTime;
+
+        //if (rb2d.velocity.y < 0)
+        if (character.velocity.y < 0)
         {
             anim.SetBool("falling", true);
             anim.SetBool("jumping", false);
@@ -44,27 +59,37 @@ public class PlayerController : MonoBehaviour
 
         if (spacePressed && !isGameOver)
         {
+            direction = Vector3.up * jumpForce;
             anim.SetBool("jumping", true);
             anim.SetBool("grounded", false);
             anim.SetBool("running", false);
-            rb2d.AddForce(Vector2.up * jumpForce);
+            //rb2d.AddForce(Vector3.up * jumpForce);
             jumpCount += 1;
             spacePressed = false;
         }
 
         if (mashKey)
         {
+            direction = Vector3.right * runSpeed;
             anim.SetBool("running", true);
-            rb2d.AddForce(transform.right * speed);
+            //rb2d.AddForce(transform.right * runSpeed);
             mashKey = false;
+        }
+
+        if(!isGameOver)
+        {
+            character.Move(direction * Time.deltaTime);
         }
     }
 
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("ground"))
-        {
+        //if (collision.gameObject.CompareTag("ground"))
+        if (character.isGrounded) {
+            Debug.Log("is she grounded?");
+            direction = Vector3.down;
+            //rb2d.AddForce(Vector3.down);
             anim.SetBool("grounded", true);
             anim.SetBool("falling", false);
             jumpCount = 0;
@@ -98,9 +123,9 @@ public class PlayerController : MonoBehaviour
         isGameOver = true;
         StopCoroutine("IncreaseGameSpeed");
         ShowGameOverPanel();
-        //increase = GameObject.Find("ScoreDetector").GetComponent<ScoreSystem>().score;
+
+
         speeds = GameObject.Find("ScoreDetector").GetComponent<ScoreSystem>().score;
-        //speeds = (int) Mathf.Ceil((increase / 2));
         Debug.Log("speeds score: " + speeds);
         int newspeed = PlayerPrefs.GetInt("speed") + speeds;
         PlayerPrefs.SetInt("speed", newspeed);
